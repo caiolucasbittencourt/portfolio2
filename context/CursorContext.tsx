16bc256a56c22cd1564449be8a8d7bc4cff2e4ce
caiolucasbittencourt <caiolucasbittencourt@hotmail.com>
2025-10-29 22:24:03 -0300

    fix: correct context logic

--- context/CursorContext.tsx
+++ context/CursorContext.tsx
@@ -1,18 +1,13 @@
-// context/CursorContext.tsx
 "use client";
 
 import { createContext, useState, useContext, ReactNode } from "react";
 
-// Define o tipo de dados que nosso contexto irá fornecer
 interface CursorContextType {
   isBlack: boolean;
   setIsBlack: (isBlack: boolean) => void;
 }
 
-// Cria o contexto com um valor padrão
 const CursorContext = createContext<CursorContextType | null>(null);
-
-// Cria o Provedor, que irá gerenciar o estado
 export function CursorProvider({ children }: { children: ReactNode }) {
   const [isBlack, setIsBlack] = useState(false);
 
@@ -23,7 +18,6 @@ export function CursorProvider({ children }: { children: ReactNode }) {
   );
 }
 
-// Cria um hook customizado para usar o contexto facilmente
 export function useCursor() {
   const context = useContext(CursorContext);
   if (!context) {
